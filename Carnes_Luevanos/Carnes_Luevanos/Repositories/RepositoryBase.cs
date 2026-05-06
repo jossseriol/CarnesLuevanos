5ded0d4373a6340051179a3c7031a62e016195c0
Anzo Seriol <joss.seriol.1705@gmail.com>
2026-05-05 21:46:17 -0600

    Subimos Archivos

---
+++ Carnes_Luevanos/Carnes_Luevanos/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Carnes_Luevanos.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server=(local); Database=MVVMLoginDb; Integrated Security=true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+
+}
---
+++ Luevanos/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Luevanos.View;
+using Luevanos.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Luevanos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainView();
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
+        }
+    }
+}
